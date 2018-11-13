ba8373cc96086eb9d697c419416cd191546a7fb9
nikhil-muskowl <40259275+nikhil-muskowl@users.noreply.github.com>
2018-11-13 14:57:16 +0530

    nik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+declare var Pushy: any;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +18,34 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      document.addEventListener('deviceready', function () {
+        // Start the Pushy service
+        Pushy.listen();
+        Pushy.requestStoragePermission();
+
+        Pushy.register(function (err, deviceToken) {
+          // Handle registration errors
+          if (err) {
+            return console.log(err);
+          }
+
+          // Display an console with device token
+          console.log('Pushy device token: ' + deviceToken);
+
+          // Send the token to your backend server via an HTTP GET request
+          //await fetch('https://your.api.hostname/register/device?token=' + deviceToken);
+
+          // Succeeded, optionally do something to alert the user
+        });
+      });
+
+      Pushy.setNotificationListener(function (data) {
+        // Print notification payload data
+        console.log('Received notification: ' + JSON.stringify(data));
+    
+        // Display an alert with the "message" payload value
+        console.log(data.message);
+    });
     });
   }
 }
